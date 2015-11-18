237e5dec357e31b619a522f9204a32a94ea0f576
Kyle Unverferth <kyle@kyleu.com>
2015-11-18 00:10:11 -0700

    Workin' on gem fusing. Still.

--- shared/src/main/scala/models/game/board/FuseHelper.scala
+++ shared/src/main/scala/models/game/board/FuseHelper.scala
@@ -5,7 +5,16 @@ import models.game.gem.Gem
 
 trait FuseHelper { this: Board =>
   def fuse() = mapGems { (gem, x, y) =>
-    val (width, height) = expand(gem, x, y)
+    val (width, height) = if(gem.width.getOrElse(1) == 1 && gem.height.getOrElse(1) == 1) {
+      if(canFuse(gem, x + 1, y) && canFuse(gem, x, y + 1) && canFuse(gem, x + 1, y + 1)) {
+        largestSize(gem, x, y, 2, 2)
+      } else {
+        1 -> 1
+      }
+    } else {
+      largestSize(gem, x, y, gem.width.getOrElse(1), gem.height.getOrElse(1))
+    }
+
     if(width > gem.width.getOrElse(1) || height > gem.height.getOrElse(1)) {
       val removals = (0 until height).flatMap { yOffset =>
         (0 until width).flatMap { xOffset =>
@@ -27,15 +36,51 @@ trait FuseHelper { this: Board =>
     }
   }.flatten
 
-  private[this] def expand(gem: Gem, x: Int, y: Int): (Int, Int) = {
+  private[this] def largestSize(gem: Gem, x: Int, y: Int, width: Int, height: Int) = {
+    val checkedDimensions = collection.mutable.HashSet.empty[(Int, Int)]
+
+    def expand(gem: Gem, x: Int, y: Int, width: Int, height: Int): (Int, Int) = {
+      println(s"width: $width, height: $height")
+      checkedDimensions += (width -> height)
+
+      val expandsUp = !(0 until width).exists { xOffset =>
+        !canFuse(gem, x + xOffset, y + height)
+      }
+      val upResult = if (expandsUp) {
+        if (checkedDimensions(width -> (height + 1))) {
+          width -> (height + 1)
+        } else {
+          expand(gem, x, y, width, height + 1)
+        }
+      } else {
+        width -> height
+      }
+
+      val expandsRight = !(0 until height).exists { yOffset =>
+        !canFuse(gem, x + width, y + yOffset)
+      }
+      val rightResult = if (expandsRight) {
+        if (checkedDimensions((width + 1) -> height)) {
+          (width + 1) -> height
+        } else {
+          expand(gem, x, y, width + 1, height)
+        }
+      } else {
+        width -> height
+      }
+
+      if ((upResult._1 * upResult._2) > (rightResult._1 * rightResult._2)) {
+        upResult
+      } else {
+        rightResult
+      }
+    }
+
     expand(gem, x, y, gem.width.getOrElse(1), gem.height.getOrElse(1))
   }
 
-  private[this] def expand(gem: Gem, x: Int, y: Int, width: Int, height: Int): (Int, Int) = {
-    if(x == 0 && y == 0) {
-      (3, 3)
-    } else {
-      (width, height)
-    }
+  private[this] def canFuse(gem: Gem, x: Int, y: Int) = at(x, y) match {
+    case Some(g) => (!g.crash) && g.timer.isEmpty && g.color == gem.color
+    case None => false
   }
 }
