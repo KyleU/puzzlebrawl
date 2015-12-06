d84fbe869497129d1bf923b96280dabbb57f390e
Kyle Unverferth <kyle@kyleu.com>
2015-12-05 23:04:59 -0700

    Fixed gem rotation, all tests pass again.

--- shared/src/main/scala/models/player/ActiveGemRotationHelper.scala
+++ shared/src/main/scala/models/player/ActiveGemRotationHelper.scala
@@ -31,40 +31,13 @@ trait ActiveGemRotationHelper { this: Player =>
   }
 
   private[this] def setIfPossible(a: GemLocation, b: GemLocation, bXDiff: Int, bYDiff: Int) = {
-    def withDelta(xDelta: Int = 0, yDelta: Int = 0) = Seq(
-      a.copy(x = a.x + xDelta, y = a.y + yDelta),
-      b.copy(x = b.x + bXDiff + xDelta, y = b.y + bYDiff + yDelta)
-    )
-
-    val newActiveGems = if (board.isValid(b.x + bXDiff, b.y + bYDiff)) {
-      Some(Seq(a, b.copy(x = b.x + bXDiff, y = b.y + bYDiff)))
-    } else {
-      val first = if (bXDiff > 0) { withDelta(xDelta = -1) } else { withDelta(xDelta = 1) }
-      val firstOk = !first.exists(g => !board.isValid(g.x, g.y))
-      if (firstOk) {
-        Some(first)
-      } else {
-        val second = if (bXDiff > 0) { withDelta(xDelta = 1) } else { withDelta(xDelta = -1) }
-        val secondOk = !second.exists(g => !board.isValid(g.x, g.y))
-        if (secondOk) {
-          Some(second)
-        } else {
-          val down = withDelta(yDelta = -1)
-          val downOk = !down.exists(g => !board.isValid(g.x, g.y))
-          if (downOk) {
-            Some(down)
-          } else {
-            None //Seq(a.copy(gem = b.gem), b.copy(gem = a.gem))
-          }
-        }
-      }
-    }
+    val newActiveGems = closestMatch(a, b, bXDiff, bYDiff)
 
     newActiveGems.flatMap { newGems =>
       val mutations = newGems.flatMap { ng =>
         val og = activeGems.find(_.gem.id == ng.gem.id).getOrElse(throw new IllegalStateException())
         val delta = (ng.x - og.x) -> (ng.y - og.y)
-        if (delta._1 > 0 || delta._2 > 0) {
+        if (delta._1 != 0 || delta._2 != 0) {
           Some(MoveGem(og.x, og.y, delta._1, delta._2))
         } else {
           None
@@ -76,6 +49,39 @@ trait ActiveGemRotationHelper { this: Player =>
         val ret = board.applyMutation(MoveGems(mutations))
         activeGems = newGems
         Some(ret)
+      }
+    }
+  }
+
+  private[this] def closestMatch(a: GemLocation, b: GemLocation, bXDiff: Int, bYDiff: Int) = {
+    def withDelta(xDelta: Int = 0, yDelta: Int = 0) = Seq(
+      a.copy(x = a.x + xDelta, y = a.y + yDelta),
+      b.copy(x = b.x + bXDiff + xDelta, y = b.y + bYDiff + yDelta)
+    )
+
+    val test = withDelta()
+    val testOk = !test.exists(g => !board.isValid(g.x, g.y, activeGems))
+    if (testOk) {
+      Some(test)
+    } else {
+      val first = if (bXDiff > 0) { withDelta(xDelta = -1) } else { withDelta(xDelta = 1) }
+      val firstOk = !first.exists(g => !board.isValid(g.x, g.y, activeGems))
+      if (firstOk) {
+        Some(first)
+      } else {
+        val second = if (bXDiff > 0) { withDelta(xDelta = 1) } else { withDelta(xDelta = -1) }
+        val secondOk = !second.exists(g => !board.isValid(g.x, g.y, activeGems))
+        if (secondOk) {
+          Some(second)
+        } else {
+          val down = withDelta(yDelta = -1)
+          val downOk = !down.exists(g => !board.isValid(g.x, g.y, activeGems))
+          if (downOk) {
+            Some(down)
+          } else {
+            None //Seq(a.copy(gem = b.gem), b.copy(gem = a.gem))
+          }
+        }
       }
     }
   }
