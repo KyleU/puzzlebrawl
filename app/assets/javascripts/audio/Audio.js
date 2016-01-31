ac530e2dbc9ddea05d75fea3ca6a945a0896cb11
Kyle Unverferth <kyle@kyleu.com>
2016-01-31 10:24:04 -0700

    Ability to turn off already-annoying sounds.

--- app/assets/javascripts/audio/Audio.js
+++ app/assets/javascripts/audio/Audio.js
@@ -1,24 +1,29 @@
 /* global define:false */
 define([], function() {
   var Audio = function(game) {
-    this.game = game;
-    this.sound = this.game.add.audio('audio');
-    this.sound.allowMultiple = true;
+    this.enabled = false;
+    if(this.enabled) {
+      this.game = game;
+      this.sound = this.game.add.audio('audio');
+      this.sound.allowMultiple = true;
 
-    this.sound.addMarker('drop', 1, 1.0);
-    this.sound.addMarker('crash', 9, 0.1);
-    this.sound.addMarker('kaching', 10, 1.0);
-    this.sound.addMarker('pow', 17, 1.0);
-    this.sound.addMarker('move', 19, 0.3);
+      this.sound.addMarker('drop', 1, 1.0);
+      this.sound.addMarker('crash', 9, 0.1);
+      this.sound.addMarker('kaching', 10, 1.0);
+      this.sound.addMarker('pow', 17, 1.0);
+      this.sound.addMarker('move', 19, 0.3);
 
-    this.sound.addMarker('hit', 3, 0.5);
-    this.sound.addMarker('escape', 4, 3.2);
-    this.sound.addMarker('meow', 8, 0.5);
-    this.sound.addMarker('death', 12, 4.2);
+      this.sound.addMarker('hit', 3, 0.5);
+      this.sound.addMarker('escape', 4, 3.2);
+      this.sound.addMarker('meow', 8, 0.5);
+      this.sound.addMarker('death', 12, 4.2);
+    }
   };
 
   Audio.prototype.play = function(key) {
-    this.sound.play(key);
+    if(this.enabled) {
+      this.sound.play(key);
+    }
   };
 
   return Audio;
